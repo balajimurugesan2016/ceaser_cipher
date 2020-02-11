3b68b92dd040638c3a3e2d6224be91f04eb9749d
Balaji Vengatesh Murugesan <balaji.vengatesh.murugesan@sap.com>
2020-02-11 18:46:12 +0000

    adding to main class

--- src/main/java/com/cipher/ceaser/App.java
+++ src/main/java/com/cipher/ceaser/App.java
@@ -1,11 +1,25 @@
 package com.cipher.ceaser;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
+    private void readInput() {
+        File file = new File("testcases.txt");
+
+        try (Scanner sc = new Scanner(file)) {
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+
+    }
     public static void main( String[] args )
     {
         
